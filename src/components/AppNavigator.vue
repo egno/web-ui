b159aa8d01ed5a90b7bb50799d84e2ea0976272c
Alexandr Shelemetyev <shelemetyev@katren.ru>
2017-11-23 17:45:03 +0700

    mod menu

--- src/components/AppNavigator.vue
+++ src/components/AppNavigator.vue
@@ -1,14 +1,47 @@
 <template>
   <el-menu>
+    <div v-for="item in menuItems">
+      <el-submenu index="item.title" v-if="item.items">
+        <template slot="title"><span>{{ item.title }}</span></template>
+        <el-menu-item v-for="subitem in item.items">
+          {{ subitem.title }}
+        </el-menu-item>
+      </el-submenu>
+      <el-menu-item v-else>
+        {{ item.title }}
+      </el-menu-item>
+    </div>
   </el-menu>
 </template>
 
 <script>
-import { Menu } from 'element-ui'
+import { Menu, MenuItem, Submenu } from 'element-ui'
 
 export default {
   components: {
-    'el-menu': Menu
+    'el-menu': Menu,
+    'el-menu-item': MenuItem,
+    'el-sumbenu': Submenu
+  },
+  data () {
+    return {
+      menuItems: [
+        {
+          title: 'Item 1'
+        },
+        {
+          title: 'Item 2',
+          items: [
+            {
+              title: 'Sumitem 2-1'
+            }
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
+
   }
 }
 </script>

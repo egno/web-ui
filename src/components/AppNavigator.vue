f4f48d1e3190439d86a7b6b00ae9eda6e0d5c650
Alexandr Shelemetyev <shelemetyev@katren.ru>
2017-11-23 17:45:03 +0700

    mod menu

--- src/components/AppNavigator.vue
+++ src/components/AppNavigator.vue
@@ -1,14 +1,68 @@
 <template>
-  <el-menu>
+  <el-menu default-active="2" class="el-menu-vertical-demo" :collapse="false">
+    <div v-for="(item, index) in menuItems">
+      <navigator-menu-item :item="item" :index="index">
+      </navigator-menu-item>
+    </div>
   </el-menu>
 </template>
 
 <script>
 import { Menu } from 'element-ui'
+import NavigatorMenuItem from '@/components/NavigatorMenuItem'
 
 export default {
   components: {
-    'el-menu': Menu
+    'el-menu': Menu,
+    'navigator-menu-item': NavigatorMenuItem
+  },
+  data () {
+    return {
+      menuItems: [
+        {
+          title: 'Item 1',
+          items: [
+            {
+              title: 'Sumitem 1-1'
+            },
+            {
+              title: 'Sumitem 1-2',
+              items: [
+                {
+                  title: 'Sumitem 1-2-1'
+                },
+                {
+                  title: 'Sumitem 1-2-2'
+                },
+                {
+                  title: 'Sumitem 1-2-3'
+                }
+              ]
+            },
+            {
+              title: 'Sumitem 1-3'
+            }
+          ]
+        },
+        {
+          title: 'Item 2',
+          items: [
+            {
+              title: 'Sumitem 2-1'
+            },
+            {
+              title: 'Sumitem 2-2'
+            },
+            {
+              title: 'Sumitem 2-3'
+            }
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
+
   }
 }
 </script>
